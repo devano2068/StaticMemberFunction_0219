9b13c8072cfdc12ce0adf5e331f91a860f08611a
Devano <devano27921@gmail.com>
2026-05-04 19:48:53 +0700

    class buku

--- chainfungctioncall.cpp
+++ chainfungctioncall.cpp
@@ -1,16 +1,4 @@
 
-    string judul;
-
-    public:
-    buku &setJudul(string judul){
-        this->judul = judul;
-        return *this;   //chain function
-    }
-    string getJudul(){
-        return this->judul;
-    }
-
-};
 
 int main()
 {
